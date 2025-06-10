56cf0f059b1b61e1dc7980ff2b45e8d56f3111ff
pavi200516 <pavithrabalajisaravanan16@gmail.com>
2025-06-10 14:16:16 +0530

    Create blood_donation.java

---
+++ DAY1/blood_donation.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Enter your age");
+        Scanner s=new Scanner(System.in);
+        int age=s.nextInt();
+        System.out.println("Enter your Weight");
+        int Weight=s.nextInt();
+        if(age>=18 && Weight>=50)Add commentMore actions
+        System.out.println("eligible for blood donation");
+        else
+        System.out.println("Not eligible for blood donation");
+        
+        
+    }
+}
