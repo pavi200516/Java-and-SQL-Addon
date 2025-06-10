96925f3e4c307184865b422c13be3a1700284497
pavi200516 <pavithrabalajisaravanan16@gmail.com>
2025-06-10 14:28:49 +0530

    Create CODE-4.java

---
+++ DAY1/DAY-2/CODE-4.java
@@ -0,0 +1,15 @@
+package program;
+
+public class demo1 {
+	public static void main(String[] args) {
+		int n=5;
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<i;j++) {Add commentMore actions
+				System.out.print(i);
+	
+			}
+			System.out.println(i);
+		}
+	}
+
+}
